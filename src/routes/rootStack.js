79e976f36aab285325f7b673661eed29ae35a1b1
BrunoOlivera123 <beenicontact@gmail.com>
2026-08-17 22:46:17 -0300

    At1708

--- src/routes/rootStack.js
+++ src/routes/rootStack.js
@@ -1,19 +1,58 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import HomeScreen from '../telas/Home'; 
+import HomeScreen from '../telas/home';
 import Login from '../telas/login';
-import CadUsuario from '../telas/cadusuario';
+import CadUsuario from '../telas/cadUsuario';
 import RecSenha from '../telas/recSenha';
+import MyTabs from './myTabs';
 
 const Stack = createNativeStackNavigator();
 
 export default function RootStack() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name= "Login" component={Login} />
-      <Stack.Screen name= "Home" component={HomeScreen} />
-      <Stack.Screen name= "CadUsuario" component={CadUsuario} />
-      <Stack.Screen name= "RecSenha" component={RecSenha} />
+    <Stack.Navigator initialRouteName="login"
+      screenOptions={
+        {
+          headerStyle: {
+            backgroundColor: '#4169E1',
+            fontWeight: 'bold',
+          },
+        }
+      }
+    >
+      <Stack.Screen
+        name="login"
+        component={Login}
+        options={{ title: 'Pagina de login' }}
+      />
+      <Stack.Screen
+        name="home"
+        component={MyTabs}
+        options={{
+          title: 'Pagina inicial',
+          headerShown: false
+          }
+        }
+      />
+      <Stack.Screen
+        name="cadUsuario"
+        component={CadUsuario}
+        options={{ title: 'Cadastro de usuario' }}
+      />
+      <Stack.Screen
+        name="recSenha"
+        component={RecSenha}
+        options={{
+          title: 'Recuperar senha',
+          headerStyle: {
+            backgroundColor: '#ff2828',
+          },
+          headerTintColor: '#ffffff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
     </Stack.Navigator>
   );
 }
--- src/telas/login.js
+++ src/telas/login.js
@@ -1,9 +1,23 @@
-import { View , Text }  from 'react-native';
+import { View , Text , Pressable }  from 'react-native';
+import { Link , useNavigation } from '@react-navigation/native';
+
+import { Button } from '@react-navigation/elements';
+
 
 export default function Login () {
+
+    const navigation = useNavigation();
+
     return (
         <View> 
             <Text> Login </Text>    
+            <Button screen="home"> Acessar o sistema</Button>
+            <Link screen="recSenha"> Esqueci a senha </Link>
+            <Pressable
+                onPress={() => navigation.navigate('cadUsuario')}
+            > 
+                <Text> Cadastrar Usuario </Text>
+            </Pressable>    
         </View>
     );
 }
